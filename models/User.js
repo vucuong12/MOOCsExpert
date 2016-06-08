7176f84a27a447d7f13128d61024a529a0effecf
vucuong12 <colddesert12@gmail.com>
2016-06-08 23:18:22 +0900

    fix conflict

--- models/User.js
+++ models/User.js
@@ -36,7 +36,7 @@ User.add({
 	currentUniversity: {type:Types.Text},
 	myCourses: {type: Types.TextArray},
 	myPosts: {type: Types.TextArray},
-	myChallenges: {type: Types.TextArray}
+	myChallenges: {type: Types.TextArray},
 	profilePicture: {type: Types.Text, default:"http://www.twiisty.com/Uploads/Profile/default_profile_pic.png"}
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
